38019b1d2ec11b6c00682706a922b7467e838c72
IAP-Reloaded <IAP-Reloaded@users.noreply.github.com>
2016-06-08 23:59:58 -0400

    Added ability to theme

--- Cloud Browser/AppDelegate.m
+++ Cloud Browser/AppDelegate.m
@@ -19,6 +19,26 @@
     NSURL*url=[NSURL URLWithString:@"http://www.google.com"];
     NSURLRequest*request=[NSURLRequest requestWithURL:url];
     [[_webView mainFrame] loadRequest:request];
+    _window.titlebarAppearsTransparent = true; // gives it "flat" look
+    _window.backgroundColor = NSColor.windowBackgroundColor; // set the background color
+
+}
+- (IBAction)setThemeDefault:(id)sender {
+    _window.appearance = [NSAppearance appearanceNamed:NSAppearanceNameVibrantLight];
+    _window.titlebarAppearsTransparent = true; // gives it "flat" look
+    _window.backgroundColor = NSColor.windowBackgroundColor; // set the background color
+
+}
+- (IBAction)setThemeDark:(id)sender {
+    _window.appearance = [NSAppearance appearanceNamed:NSAppearanceNameVibrantDark];
+    _window.titlebarAppearsTransparent = true; // gives it "flat" look
+    _window.backgroundColor = NSColor.windowBackgroundColor; // set the background color
+
+}
+- (IBAction)setThemeAqua:(id)sender {
+    _window.appearance = [NSAppearance appearanceNamed:NSAppearanceNameAqua];
+    _window.titlebarAppearsTransparent = true; // gives it "flat" look
+    _window.backgroundColor = NSColor.windowBackgroundColor; // set the background color
 
 }
 
